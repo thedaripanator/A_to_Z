a9ff0238580678d2ad8a3f19ff0fb1046d43f8cc
thedaripanator <sayondeep1618@gmail.com>
2026-08-27 18:15:47 +0530

    Solved the kadanes Algorithm and Stock Buy and Sell Problem

---
+++ Arrays/Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Best_Time_to_Buy_and_Sell_Stock {
+    public static int maxProfit(int[] arr) {
+         int buy=arr[0];
+         int max=0;
+         for(int i=1;i<arr.length;i++){
+             if(arr[i] < buy){
+                 buy=arr[i];
+             }
+             else{
+                 max=Math.max(max,arr[i]-max);
+             }
+         }
+         return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(maxProfit(arr));
+    }
+}
---
+++ Arrays/Kedanes_Algorithm.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Kedanes_Algorithm {
+    public static int maxSubArray(int[] nums) {
+       int sum=nums[0];
+       int max=nums[0];
+       for(int i=1;i<nums.length;i++){
+           sum=Math.max(nums[i],sum+nums[i]);
+           max=Math.max(sum,max);
+       }
+       return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxSubArray(arr));
+    }
+}
